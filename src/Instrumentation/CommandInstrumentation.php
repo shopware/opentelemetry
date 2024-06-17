3712ef7fc649294ae2b21979e6be70d677197288
Soner Sayakci <s.sayakci@gmail.com>
2024-06-17 13:38:23 +0200

    feat: distributed tracing

---
+++ src/Instrumentation/CommandInstrumentation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Shopware\OpenTelemetry\Instrumentation;
+
+use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\Context\Context;
+use Opentelemetry\Proto\Trace\V1\Span\SpanKind;
+use Opentelemetry\Proto\Trace\V1\Status\StatusCode;
+use OpenTelemetry\SemConv\TraceAttributes;
+use Symfony\Component\Console\Application;
+use Throwable;
+use function OpenTelemetry\Instrumentation\hook;
+
+class CommandInstrumentation
+{
+    public static function register()
+    {
+        hook(
+            class: Application::class,
+            function: 'doRunCommand',
+            pre: static function (
+                Application $application,
+                array       $params,
+                string      $class,
+                string      $function,
+                ?string     $filename,
+                ?int        $lineno,
+            ) {
+                $builder = (new CachedInstrumentation('io.opentelemetry.contrib.php.shopware'))
+                    ->tracer()
+                    ->spanBuilder(sprintf('bin/console %s', $params[0]->getName()))
+                    ->setSpanKind(SpanKind::SPAN_KIND_INTERNAL)
+                    ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
+                    ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
+                    ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
+                    ->setAttribute(TraceAttributes::CODE_LINENO, $lineno);
+
+                $parent = Context::getCurrent();
+
+                $span = $builder
+                    ->setParent($parent)
+                    ->startSpan();
+
+                Context::storage()->attach($span->storeInContext($parent));
+            },
+            post: static function (
+                Application $application,
+                array       $params,
+                mixed       $ret,
+                ?Throwable  $exception
+            ) {
+                $scope = Context::storage()->scope();
+                if (null === $scope) {
+                    return;
+                }
+                $scope->detach();
+                $span = Span::fromContext($scope->context());
+                if ($exception !== null) {
+                    $span->setStatus(StatusCode::STATUS_CODE_ERROR, $exception->getMessage());
+                }
+                $span->end();
+            }
+        );
+    }
+}
---
+++ src/Messenger/EnvelopePropagator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Shopware\OpenTelemetry\Messenger;
+
+use OpenTelemetry\Context\Propagation\PropagationGetterInterface;
+use OpenTelemetry\Context\Propagation\PropagationSetterInterface;
+use function assert;
+
+class EnvelopePropagator implements PropagationSetterInterface, PropagationGetterInterface
+{
+    public static function instance(): self
+    {
+        static $instance;
+
+        return $instance ??= new self();
+    }
+
+    /**
+     * @param EnvelopeStamp $carrier
+     */
+    public function set(&$carrier, string $key, string $value): void
+    {
+        assert($carrier instanceof EnvelopeStamp);
+
+        $carrier->data[$key] = $value;
+    }
+
+    public function keys($carrier): array
+    {
+        assert($carrier instanceof EnvelopeStamp);
+
+        return array_keys($carrier->data);
+    }
+
+    public function get($carrier, string $key): ?string
+    {
+        assert($carrier instanceof EnvelopeStamp);
+
+        return $carrier->data[$key] ?? null;
+    }
+}
