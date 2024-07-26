db9bbcd5967bf5bec0856efc38bc9884fe99dc05
Andrii Havryliuk <a.havryliuk@shopware.com>
2024-07-26 18:02:10 +0200

    added OpenTelemetry transport for metrics

--- src/OpenTelemetryShopwareBundle.php
+++ src/OpenTelemetryShopwareBundle.php
@@ -2,15 +2,25 @@
 
 namespace Shopware\OpenTelemetry;
 
-use OpenTelemetry\API\Globals;
+use OpenTelemetry\API\Metrics\MeterInterface;
 use Shopware\OpenTelemetry\Logging\OpenTelemetryLoggerFactory;
 use Shopware\OpenTelemetry\Messenger\MessageBusSubscriber;
+use Shopware\OpenTelemetry\Metrics\Transports\OpenTelemetryMeterFactory;
+use Shopware\OpenTelemetry\Metrics\Transports\OpenTelemetryMetricTransport;
 use Shopware\OpenTelemetry\Profiler\OtelProfiler;
 use OpenTelemetry\Contrib\Logs\Monolog\Handler;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class OpenTelemetryShopwareBundle extends Bundle
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+/**
+ * @phpstan-type Config array{metrics: array{enabled: bool, namespace: string}}
+ */
+class OpenTelemetryShopwareBundle extends AbstractBundle
 {
     public function build(ContainerBuilder $container)
     {
@@ -26,6 +36,50 @@ class OpenTelemetryShopwareBundle extends Bundle
             $container
                 ->register('monolog.handler.open_telemetry', Handler::class)
                 ->setFactory([OpenTelemetryLoggerFactory::class, 'build']);
+        }
+
+    }
+
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $rootNode = $definition->rootNode();
+        \assert($rootNode instanceof ArrayNodeDefinition);
+
+        $rootNode
+            ->children()
+                ->arrayNode('metrics')
+                    ->children()
+                        ->booleanNode('enabled')
+                            ->defaultFalse()
+                        ->end()
+                        ->scalarNode('namespace')
+                            ->defaultValue('io.opentelemetry.contrib.php.shopware')
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    /**
+     * @param Config $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $metricsConfig = $config['metrics'];
+        $namespace = $metricsConfig['namespace'];
+
+        if ($metricsConfig['enabled']) {
+            $container->services()
+                ->set(MeterInterface::class)
+                ->factory([OpenTelemetryMeterFactory::class, 'createMeter'])
+                ->arg('$namespace', $namespace);
+
+            $container->services()
+                ->set(OpenTelemetryMetricTransport::class)
+                ->arg('$meter', service(MeterInterface::class))
+                ->arg('$namespace', $namespace)
+                ->tag('shopware.metric_transport');
         }
     }
 }
