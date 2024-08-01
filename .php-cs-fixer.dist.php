d1460e63396cfb6aa723ded04a0246240433da10
Andrii Havryliuk <a.havryliuk@shopware.com>
2024-08-01 13:56:37 +0200

    added php-cs-fixer github workflow

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Pipeline of the project is using  https://github.com/shopware/github-actions/blob/main/.github/workflows/cs-fixer.yml
+ *
+ * This file is added to make it easier to run php-cs-fixer during development.
+ *
+ */
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PER-CS2.0' => true,
+        'no_unused_imports' => true,
+        'declare_strict_types' => true,
+    ])
+    ->setFinder(
+        (new Finder())
+            ->in(__DIR__)
+    )
+    ;
