dca1bfb177ea07d1aeb85bb5bdec2981623567d4
Ghaith Olabi <m.olabi@shopware.com>
2024-08-27 11:31:52 +0200

    feat: add event dispatcher instrumentation

--- src/Instrumentation/SymfonyInstrumentation.php
+++ src/Instrumentation/SymfonyInstrumentation.php
@@ -10,6 +10,7 @@ use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
 use Shopware\Core\Framework\Adapter\Kernel\HttpKernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Throwable;
 use OpenTelemetry\SemConv\TraceAttributes;
@@ -25,6 +26,7 @@ final class SymfonyInstrumentation
     public static function register(): void
     {
         self::instrumentKernel();
+        self::instrumentEventDispatcher();
     }
 
     private static function instrumentKernel(): void
@@ -158,6 +160,60 @@ final class SymfonyInstrumentation
                     ->setStatus(StatusCode::STATUS_ERROR, $throwable->getMessage());
 
                 return $params;
+            },
+        );
+    }
+
+    private static function instrumentEventDispatcher(): void
+    {
+        hook(
+            EventDispatcher::class,
+            'dispatch',
+            pre: static function (
+                EventDispatcher $dispatcher,
+                array           $params,
+                string          $class,
+                string          $function,
+                ?string         $filename,
+                ?int            $lineno,
+            ) {
+                $instrumentation = new CachedInstrumentation('io.opentelemetry.contrib.php.symfony');
+                $eventClass = $params[0]::class;
+                $name = sprintf('EventDispatcher::dispatch(%s)', $eventClass);
+
+                $builder = $instrumentation
+                    ->tracer()
+                    ->spanBuilder($name)
+                    ->setSpanKind(SpanKind::KIND_INTERNAL)
+                    ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
+                    ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
+                    ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
+                    ->setAttribute(TraceAttributes::CODE_LINENO, $lineno)
+                    ->setAttribute(TraceAttributes::EVENT_NAME, $eventClass);
+
+                $parent = Context::getCurrent();
+                $span = $builder->setParent($parent)->startSpan();
+                Context::storage()->attach($span->storeInContext($parent));
+            },
+            post: static function (
+                EventDispatcher $dispatcher,
+                array           $params,
+                object         $return,
+                ?\Throwable     $exception,
+            ) {
+                $scope = Context::storage()->scope();
+                if (null === $scope) {
+                    return;
+                }
+                $scope->detach();
+                $span = Span::fromContext($scope->context());
+
+                if ($exception !== null) {
+                    $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
+                    $span->recordException($exception);
+                }
+
+                $span->end();
             },
         );
     }
