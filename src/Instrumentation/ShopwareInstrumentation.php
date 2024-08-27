7822e6b2acded36b66c40e6a619e0f36a55dde31
Ghaith Olabi <m.olabi@shopware.com>
2024-08-27 09:46:19 +0200

    fix: move distributed instrumentation handling to Core Kernel

--- src/Instrumentation/ShopwareInstrumentation.php
+++ src/Instrumentation/ShopwareInstrumentation.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Shopware\OpenTelemetry\Instrumentation;
 
+use OpenTelemetry\API\Globals;
 use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
 use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\SpanInterface;
 use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
@@ -13,6 +15,7 @@ use OpenTelemetry\SemConv\TraceAttributes;
 use Shopware\Core\Framework\Script\Execution\ScriptExecutor;
 use Shopware\Core\Kernel;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Throwable;
 
@@ -173,27 +176,46 @@ class ShopwareInstrumentation
                     ->setAttribute(TraceAttributes::CODE_LINENO, $lineno);
 
                 $parent = Context::getCurrent();
-                $span = $builder->startSpan();
+                if ($request) {
+                    $parent = Globals::propagator()->extract($request, RequestPropagationGetter::instance());
+                    $span = $builder
+                        ->setParent($parent)
+                        ->setAttribute(TraceAttributes::URL_FULL, $request->getUri())
+                        ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $request->getMethod())
+                        ->setAttribute(TraceAttributes::HTTP_REQUEST_BODY_SIZE, $request->headers->get('Content-Length'))
+                        ->setAttribute(TraceAttributes::URL_SCHEME, $request->getScheme())
+                        ->setAttribute(TraceAttributes::URL_PATH, $request->getPathInfo())
+                        ->setAttribute(TraceAttributes::USER_AGENT_ORIGINAL, $request->headers->get('User-Agent'))
+                        ->setAttribute(TraceAttributes::SERVER_ADDRESS, $request->getHost())
+                        ->setAttribute(TraceAttributes::SERVER_PORT, $request->getPort())
+                        ->startSpan();
+                    $request->attributes->set(SpanInterface::class, $span);
+                } else {
+                    $span = $builder->startSpan();
+                }
 
                 Context::storage()->attach($span->storeInContext($parent));
+                return [$request];
             },
             post: static function (
                 Kernel     $kernel,
-                array      $params,
-                mixed      $ret,
+                array $params,
+                ?Response $response,
                 ?Throwable $exception,
             ) {
                 $scope = Context::storage()->scope();
                 if (null === $scope) {
                     return;
                 }
+                $scope->detach();
                 $span = Span::fromContext($scope->context());
+
                 if ($exception !== null) {
                     $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
                     $span->recordException($exception);
                 }
+
                 $span->end();
-                $scope->detach();
             },
         );
     }
--- src/Instrumentation/SymfonyInstrumentation.php
+++ src/Instrumentation/SymfonyInstrumentation.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Shopware\OpenTelemetry\Instrumentation;
 
-use OpenTelemetry\API\Globals;
 use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
 use OpenTelemetry\API\Trace\Span;
-use OpenTelemetry\API\Trace\SpanInterface;
 use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
@@ -26,17 +24,22 @@ final class SymfonyInstrumentation
 {
     public static function register(): void
     {
+        self::instrumentKernel();
+    }
+
+    private static function instrumentKernel(): void
+    {
         hook(
             HttpKernel::class,
             'handle',
             pre: static function (
                 HttpKernel $kernel,
-                array $params,
-                string $class,
-                string $function,
-                ?string $filename,
-                ?int $lineno,
-            ): array {
+                array      $params,
+                string     $class,
+                string     $function,
+                ?string    $filename,
+                ?int       $lineno,
+            ) {
                 $instrumentation = new CachedInstrumentation('io.opentelemetry.contrib.php.symfony');
                 $request = ($params[0] instanceof Request) ? $params[0] : null;
                 $type = $params[1] ?? HttpKernelInterface::MAIN_REQUEST;
@@ -62,31 +65,14 @@ final class SymfonyInstrumentation
                     ->setAttribute(TraceAttributes::CODE_LINENO, $lineno);
 
                 $parent = Context::getCurrent();
-                if ($request) {
-                    $parent = Globals::propagator()->extract($request, RequestPropagationGetter::instance());
-                    $span = $builder
-                        ->setParent($parent)
-                        ->setAttribute(TraceAttributes::URL_FULL, $request->getUri())
-                        ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $request->getMethod())
-                        ->setAttribute(TraceAttributes::HTTP_REQUEST_BODY_SIZE, $request->headers->get('Content-Length'))
-                        ->setAttribute(TraceAttributes::URL_SCHEME, $request->getScheme())
-                        ->setAttribute(TraceAttributes::URL_PATH, $request->getPathInfo())
-                        ->setAttribute(TraceAttributes::USER_AGENT_ORIGINAL, $request->headers->get('User-Agent'))
-                        ->setAttribute(TraceAttributes::SERVER_ADDRESS, $request->getHost())
-                        ->setAttribute(TraceAttributes::SERVER_PORT, $request->getPort())
-                        ->startSpan();
-                    $request->attributes->set(SpanInterface::class, $span);
-                } else {
-                    $span = $builder->startSpan();
-                }
-                Context::storage()->attach($span->storeInContext($parent));
 
-                return [$request];
+                $span = $builder->setParent($parent)->startSpan();
+                Context::storage()->attach($span->storeInContext($parent));
             },
             post: static function (
                 HttpKernel $kernel,
-                array $params,
-                ?Response $response,
+                array      $params,
+                ?Response  $response,
                 ?Throwable $exception,
             ): void {
                 $scope = Context::storage()->scope();
@@ -156,11 +142,11 @@ final class SymfonyInstrumentation
             'handleThrowable',
             pre: static function (
                 HttpKernel $kernel,
-                array $params,
-                string $class,
-                string $function,
-                ?string $filename,
-                ?int $lineno,
+                array      $params,
+                string     $class,
+                string     $function,
+                ?string    $filename,
+                ?int       $lineno,
             ): array {
                 /** @var \Throwable $throwable */
                 $throwable = $params[0];
